2f7b36d42b1ce09ae61cf5e38e6cf83a88547061
unknown <hm3636690@gmail.com>
2025-10-26 20:54:54 +0500

    Loader completed successfully

---
+++ app.js
@@ -0,0 +1,34 @@
+let grow = 0;
+setInterval(() => {
+  if (grow < 101) {
+    document.querySelector(".increas").innerHTML = grow;
+    grow++;
+  } else {
+    grow;
+  }
+}, 25);
+
+let tl = gsap.timeline();
+
+tl.from(".row h1", {
+  y: 150,
+  delay: 0.5,
+  duration: 0.7,
+});
+tl.from(".row h3", {
+  opacity: 0,
+});
+
+tl.to("#loader", {
+  opacity: 0,
+  delay: 2,
+  display: "none",
+});
+
+tl.from(".hero", {
+  y: 1600,
+  delay: 0.2,
+  duration: 0.5,
+  opacity: 0,
+  ease: Power4,
+});
